2a3e9a9d8e4413ae5449629ff63fff8ec907ddf5
datdo2112001 <datdobn58211@gmail.com>
2022-06-28 02:28:53 -0700

    v 1.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
-  root "static_pages#home"
+  root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
